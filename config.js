39f32e69cfad9035665b6ae0cefff557fef534bb
Aleazar0252 <aleazarjohnvillanueva@gmail.com>
2026-05-05 01:11:39 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBBQzF1nOAzZwhGrZHbJn6ngvVDQNXoM1k",
+  authDomain: "test-auth-208e8.firebaseapp.com",
+  projectId: "test-auth-208e8",
+  storageBucket: "test-auth-208e8.firebasestorage.app",
+  messagingSenderId: "218078567147",
+  appId: "1:218078567147:web:24d911f854d656e747bcae",
+  measurementId: "G-KGHQRTRSY4"
+};
